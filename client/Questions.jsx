acdf7e02be7094f2df3fd39847c6c33bb30037b4
Moustafa Ismael <mismael7@gmail.com>
2016-02-29 12:13:15 +0200

    Guide the user through the short questionaire

--- client/Questions.jsx
+++ client/Questions.jsx
@@ -1,7 +1,14 @@
 
-const { Link, State, RouteHandler } = ReactRouter;
+const { Link, State, RouteHandler, History } = ReactRouter;
 
 Intention = React.createClass({
+
+  mixins: [History],
+
+  /* Coming in Version 2.0 */
+  // contextTypes: {
+  //   router: React.PropTypes.object.isRequired
+  // },
 
   render() {
     return (
@@ -23,12 +30,22 @@ Intention = React.createClass({
 
 Amount = React.createClass({
 
+  setQuantity() {
+    event.preventDefault();
+
+    let quantity = ReactDOM.findDOMNode(this.refs.quantity).value.trim();
+    Reserves.insert({id: window.curUser.deviceId, amount: quantity});
+
+    // this.context.router.push('/target'); /* Coming in Version 2.0 */
+    this.props.history.pushState(null, '/target');  // PushState takes the path not Component name
+  },
+
   render() {
     return (
-      <form className="app-box" action="/target">
+      <form className="app-box" onSubmit={this.setQuantity}>
         <div className="row large-text">
           <div className="col s12 m12 l7 ">How much would you like to sell?</div>
-          <input type="text" className="col s8 m8 l3 large-input-text"/>
+          <input type="text" ref="quantity" className="col s8 m8 l3 large-input-text"/>
           <span className="col s4 m4 l2">USD</span>
         </div>
         <button className="waves-effect waves-teal btn-flat" type="submit">Next</button>
@@ -39,12 +56,25 @@ Amount = React.createClass({
 
 Target = React.createClass({
 
+  setPrice() {
+    event.preventDefault();
+
+    let curSale = Reserves.findOne({id: window.curUser.deviceId});
+
+    let price = ReactDOM.findDOMNode(this.refs.price).value.trim();
+    curSale["price"] = this.refs.price;
+
+    Reserves.update({id: window.curUser.deviceId}, curSale);
+
+    this.props.history.pushState(null, '/bids');
+  },
+
   render() {
     return (
-      <form className="app-box" action="/target">
+      <form className="app-box" onSubmit={this.setPrice}>
         <div className="row large-text">
           <div className="col s12 m12 l12">What is the <u>minimum</u> you will accept per Dollar?</div>
-          <input type="text" className="col s8 m8 l6 large-input-text"/>
+          <input type="text" refs="price" className="col s8 m8 l6 large-input-text"/>
           <span className="col s4 m2 l4">EGP</span>
         </div>
         <button className="waves-effect waves-teal btn-flat" type="submit">Next</button>
